8a1005bc697c596c6732a71fecc15fbdc3ff8f88
MohammadFaysal731 <www.faysal731@gmail.com>
2022-03-30 22:47:58 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 5555;
+
+const singerStyle = {
+  color: 'purple',
+  backgroundColor: 'white'
+}
+const singers = [
+  { name: 'Dr Mahfuz', job: 'Singer' },
+  { name: 'Eva Rahman', job: 'Singer2' },
+  { name: 'Agun', job: 'sopno' },
+  { name: 'Shuvro', job: 'pathor' },
+];
+
 function App() {
+  const nayoks = ['Rubel', 'BappaRaz', 'Kuber', 'Jashim', 'Salman Shah', 'Riyaz', 'Razzak'];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        nayoks.map(nayok => <li>Name:{nayok}</li>)
+      }
+      {/*  {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      } */}
+      {
+        singers.map(singer => <Person name={singer.name} job={singer.job}></Person>)
+      }
+      {/* <Person name={nayoks[0]} nayika="Moushumi"></Person>
+      <Person name={nayoks[1]} nayika="Cheka"></Person>
+      <Person name={nayoks[2]} nayika="Kopila"></Person> */}
+      <h5>New Component .YAY</h5>
+      <p id="totocompany">rock mama React mama</p>
+      <Friend name="Faysal" nayika="Sumaya" phone="01666"></Friend>
+      <Friend name="Faysal" nayika="Sumaya" phone="01666"></Friend>
     </div>
   );
+}
+function Person(props) {
+  // console.log(props);
+  return (
+    <div className="person">
+      <h1>{props.name}</h1>
+      <p>{props.nayika}</p>
+    </div>
+  )
+}
+
+function Friend(props) {
+  // console.log(props);
+  return (
+    <div className="container">
+      <h3>{props.name}</h3>
+      <p>Nayika:{props.nayika}</p>
+      <p>phone:{props.phone}</p>
+    </div>
+  )
 }
 
 export default App;
